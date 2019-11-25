b7b91dccd19751cd86c0dd8443be7002f2b6b985
Josiah Horton <joehorton99@gmail.com>
2019-11-25 12:10:36 -0800

    add tests

--- projects_js_2019/0101_equality-check.js
+++ projects_js_2019/0101_equality-check.js
@@ -26,3 +26,16 @@ function assertEquals(c, d){
 function checkEquality(a, b) {
   // TODO: check the quality of a and b
 }
+
+
+console.log(assertEquals(checkEquality(1, true), false));
+console.log(assertEquals(checkEquality(0, "0"), false));
+console.log(assertEquals(checkEquality(1, 1), true));
+console.log(assertEquals(checkEquality(0, ""), false));
+console.log(assertEquals(checkEquality(1, "1"), false));
+console.log(assertEquals(checkEquality(0, false), false));
+console.log(assertEquals(checkEquality(NaN, NaN), false));
+console.log(assertEquals(checkEquality(null, undefined), false));
+console.log(assertEquals(checkEquality(undefined, undefined), true));
+console.log(assertEquals(checkEquality(false, null), false));
+console.log(assertEquals(checkEquality(1, 2), false));
