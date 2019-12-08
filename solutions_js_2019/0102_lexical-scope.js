ce65da065d7e34ad34e92dc54f7cb8baf722ef74
Josiah Horton <joehorton99@gmail.com>
2019-12-08 13:14:32 -0800

    add functions from project

--- solutions_js_2019/0102_lexical-scope.js
+++ solutions_js_2019/0102_lexical-scope.js
@@ -7,3 +7,38 @@
  */
 
 
+var b = 3;
+
+ function one(){
+   // this a is specific to this scope
+   var a = 1;
+ }
+
+  one(); // 1
+
+ function two(){
+   // this a is specific to this scope
+   var a = 2;
+ }
+
+ two(); // 2
+
+ function three(){
+   // b's scope is the whole file
+ }
+
+ three(); // 3
+
+ function four(){
+   // x's scope is all of four
+   var x = 4;
+   function addOne(){
+     // y's scope is only addOne
+     var y = 1;
+   }
+
+   addOne();
+ }
+
+ four(); // 4 and 5
+
