e0c42ad34a179dddbaa9ee586e941084c395cdde
Josiah Horton <joehorton99@gmail.com>
2019-12-08 13:12:08 -0800

    create function four

--- projects_js_2019/0102_lexical-scope.js
+++ projects_js_2019/0102_lexical-scope.js
@@ -14,12 +14,20 @@ var b = 3; // this b has a global scope
    var a = 1;
    // TODO: log a
  }
+
  function two(){
    // this a is specific to this scope
    var a = 2;
    // TODO: log a
  }
+
  function three(){
    // b's scope is the whole js file
    // TODO: log b
+ }
+
+ function four(){
+   // x's scope is all of four
+   var x = 4;
+   // TODO: log x
  }
